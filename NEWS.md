7b79a7eda312c52961587ab0e2d217f7151518a8
liming <limiang929@gmail.com>
2025-09-27 16:52:47 +0800

    remove dependency of Vennerable pacakge

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# epiSeeker 0.99.1
+
++ remove dependent of `Vennerable` package (2025-09-27, Sta)
+
 # epiSeeker 0.99.0
 
 + epiSeeker inherits from ChIPseeker to support analysis of multi-omics epigenomic data (2025-09-19, Fri)
--- R/vennplot.R
+++ R/vennplot.R
@@ -1,38 +1,26 @@
 #' plot the overlap of a list of object
 #'
-#'
-#' There are two ways to plot, which users can specify through `by`.
-#' 
-#' The first way is to use `gplots` packages, by setting `by = gplots`. This method
-#' is default method. The venn plot produced through this way has no color.
-#' 
-#' The second way is to use `ggVennDiagram` packages, by setting `by = ggVennDiagram`. 
-#' The venn plot produced through this way has colors which can be defined by users using
+#' venn plot produced through this way has colors which can be defined by users using
 #' ggplot2 grammar e.g.(scale_fill_distiller()). And users can specify any details, like digital number,
 #' text size and showing percentage or not, by inputting `...` extra parameters.
 #' 
 #' @title vennplot
-#' @param Sets a list of object, can be vector or GRanges object
-#' @param by one of gplots, ggVennDiagram or Vennerable
+#' @param Sets a list of object, can be vector or GRanges object.
 #' @param ... extra parameters using ggVennDiagram. Details see \link[ggVennDiagram]{ggVennDiagram}
 #' @return venn plot that summarize the overlap of peaks
 #' from different experiments or gene annotation from
 #' different peak files.
-#' @importFrom gplots plot.venn
-#' @importFrom ggVennDiagram ggVennDiagram
-#' @importFrom Vennerable Venn
-#' @importFrom grid grid.newpage
 #' @examples
 #' require(TxDb.Hsapiens.UCSC.hg19.knownGene)
 #' txdb <- TxDb.Hsapiens.UCSC.hg19.knownGene
 #' peakfiles <- getSampleFiles()
-#' peakAnnoList <- lapply(peakfiles, annotateSeq)
+#' peakAnnoList <- lapply(peakfiles, annotateSeq, TxDb = txdb)
 #' names(peakAnnoList) <- names(peakfiles)
-#' genes= lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
-#' vennplot(genes, by = "gplots")
+#' genes <- lapply(peakAnnoList, function(i) as.data.frame(i)$geneId)
+#' vennplot(genes)
 #' @export
 #' @author G Yu
-vennplot <- function(Sets, by="gplots",...) {
+vennplot <- function(Sets,...) {
     if (is.null(names(Sets))) {
         nn <- paste0("Set", seq_along(Sets))
         warning("input is not a named list, set the name automatically to ", paste(nn, collapse = " "))
@@ -40,38 +28,13 @@ vennplot <- function(Sets, by="gplots",...) {
         ## stop("input object should be a named list...")
     }
 
-    overlapDF <- overlap(Sets)
-    if (by == "Vennerable") {
-        ## setRepositories(ind=7)
-        ## install.package("Vennerable")
-        ## OR
-        ## install.packages("Vennerable", repos="http://R-Forge.R-project.org")
-        print("using Vennerable")
-        if (requireNamespace("Vennerable", quietly = TRUE)){
-
-            Venn <- eval(parse(text="Venn"))
-            v <- Venn(SetNames=names(Sets), Weight=overlapDF$Weight)
-            plotVenn <- eval(parse(text="Vennerable:::plotVenn"))
-            plotVenn(v)
-        }
-
-        
-    } else if (by == "gplots") {
-        n <- ncol(overlapDF)
-        colnames(overlapDF)[n] <- "num"
-        overlapDF <- overlapDF[, c(n, 1:(n-1))]
-        rownames(overlapDF) <- apply(overlapDF, 1, function(i) paste(i[-1], sep="", collapse=""))
-        vennCount <- as.matrix(overlapDF)
-        class(vennCount) <- "venn"
-        plot.venn(vennCount)
-    } else if(by == "ggVennDiagram"){
-        if (requireNamespace("ggVennDiagram")){
-            ggVennDiagram::ggVennDiagram(Sets, ...)
-        }
-	    
-    } else {
-        stop("not supported...")
+    rlang::check_installed('ggVennDiagram', reason = 'For venn plot.')
+    if (requireNamespace("ggVennDiagram")){
+        p <- ggVennDiagram::ggVennDiagram(Sets, ...)
     }
+
+    return(p)
+
 }
 
 #' vennplot for peak files
